a243455e6f0cb38aec1282a8f0bdac4ad0802125
yanby <18735790101@163.com>
2018-10-22 10:06:36 +0800

    1

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-demo',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  plugins: [
+    {src:'~/plugins/element-ui',ssr:true}
+  ],
+  //全局css
+  css:[
+    {src:'~assets/css/base.css'},
+    {src:'element-ui/lib/theme-chalk/index.css'}
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*关闭ESLint
+    ** Run ESLint on save
+    */
+    // extend (config, { isDev, isClient }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+    vendor: [
+      'axios',
+      'element-ui'
+    ],
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 3 versions']
+      })
+    ]
+  }
+}
+
---
+++ pages/mine/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <el-button type="primary">主要按钮</el-button>
+    <img src="~/assets/imgs/1.png" alt="">
+  </section>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: '111111111',
+      meta: [
+        { hid: 'description', name: 'description', content: '444444444444444444444444' },
+        { hid: 'title', name: 'title', content: '33333333333333333333333333' },
+        { hid: 'keywords', name: 'keywords', content: '22222222222222222' }
+      ]
+    }
+  },
+  components: {
+
+  },
+  mounted(){
+    console.log(this.$route.params.id);
+  }
+}
+</script>
+
+
+
+<style lang="less" type="text/less" scoped>
+
+</style>
+
